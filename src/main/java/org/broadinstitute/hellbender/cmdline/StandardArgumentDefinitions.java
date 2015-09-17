laserson/hellbender

Port SelectVariants tool and dependent infrastructure.

src/main/java/org/broadinstitute/hellbender/cmdline/StandardArgumentDefinitions.java
@@ -11,12 +11,16 @@ public final class StandardArgumentDefinitions {
     public static final String REFERENCE_LONG_NAME = "reference";
     public static final String VARIANT_LONG_NAME = "variant";
     public static final String FEATURE_LONG_NAME = "feature";
+    public static final String LENIENT_LONG_NAME = "lenient";
+    public static final String INTERVAL_LONG_NAME = "interval";
 
     public static final String INPUT_SHORT_NAME = "I";
     public static final String OUTPUT_SHORT_NAME = "O";
     public static final String REFERENCE_SHORT_NAME = "R";
     public static final String VARIANT_SHORT_NAME = "V";
     public static final String FEATURE_SHORT_NAME = "F";
+    public static final String LENIENT_SHORT_NAME = "LE";
+    public static final String INTERVAL_SHORT_NAME = "IN";
     public static final String SAMPLE_ALIAS_SHORT_NAME = "ALIAS";
     public static final String LIBRARY_NAME_SHORT_NAME = "LIB";
     public static final String EXPECTED_INSERT_SIZE_SHORT_NAME = "INSERT";

yegeniy/gatk

src/main/java/org/broadinstitute/hellbender/cmdline/StandardArgumentDefinitions.java
@@ -11,16 +11,19 @@ public final class StandardArgumentDefinitions {
     public static final String REFERENCE_LONG_NAME = "reference";
     public static final String VARIANT_LONG_NAME = "variant";
     public static final String FEATURE_LONG_NAME = "feature";
+    public static final String LENIENT_LONG_NAME = "lenient";
+    public static final String INTERVAL_LONG_NAME = "interval";
 
     public static final String INPUT_SHORT_NAME = "I";
     public static final String OUTPUT_SHORT_NAME = "O";
     public static final String REFERENCE_SHORT_NAME = "R";
     public static final String VARIANT_SHORT_NAME = "V";
     public static final String FEATURE_SHORT_NAME = "F";
+    public static final String LENIENT_SHORT_NAME = "LE";
+    public static final String INTERVAL_SHORT_NAME = "L";
     public static final String SAMPLE_ALIAS_SHORT_NAME = "ALIAS";
     public static final String LIBRARY_NAME_SHORT_NAME = "LIB";
     public static final String EXPECTED_INSERT_SIZE_SHORT_NAME = "INSERT";
-    public static final String LANE_SHORT_NAME = "L";
     public static final String SEQUENCE_DICTIONARY_SHORT_NAME = "SD";
     public static final String METRICS_FILE_SHORT_NAME = "M";
     public static final String ASSUME_SORTED_SHORT_NAME = "AS";